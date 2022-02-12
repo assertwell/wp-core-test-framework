6ef20ae4ba965865cf00276d405f51a9fd669f1e
Viktor Szépe <viktor@szepe.net>
2022-02-12 05:30:49 +0000

    Improve refresh script

--- bin/refresh-wp-install-tests.sh
+++ bin/refresh-wp-install-tests.sh
@@ -3,9 +3,9 @@
 # Refreshes the bin/install-wp-tests.sh file from source, injecting a comment at the top of the file.
 
 SRC="https://raw.githubusercontent.com/wp-cli/scaffold-command/master/templates/install-wp-tests.sh"
-DIR="$( cd "$(dirname "$0")" || exit 1; pwd -P )"
+DIR="$(cd "$(dirname "$0")" || exit 1; pwd -P)"
 
 curl -s "$SRC" \
-    | sed $'2i\\\n# Notice: This file is pulled from the WP-CLI scaffold command,\\\n# and should not be modified directly.\n' \
-    > "$DIR/install-wp-tests.sh" \
-    && echo -e "\033[0;32mbin/install-wp-tests.sh has been updated to match ${SRC}\033[0;0m"
+    | sed -e '2 i # Notice: This file is pulled from the WP-CLI scaffold command,\n# and should not be modified directly.' \
+    > "${DIR}/install-wp-tests.sh" \
+    && printf '\033[0;32m%s %s\033[0;0m\n' "bin/install-wp-tests.sh has been updated to match" "${SRC}"
